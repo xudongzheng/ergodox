8bbd185f31bbe17690bd0ac4e2295ee8c16436c8
Xudong Zheng <7pkvm5aw@slicealias.com>
2018-01-08 10:21:56 +0000

    Mon, 08 Jan 2018 10:21:56 +0000

--- build.sh
+++ build.sh
@@ -3,12 +3,9 @@ set -e -o pipefail
 cd $(dirname $0)
 
 if [ ! -d qmk ]; then
-	git clone git@github.com:qmk/qmk_firmware.git
-	cd qmk_firmware
-	git checkout 7260fc3eef98fb7b0e2ed24d3d0d14cf2e613000
-	cd ..
-	mv qmk_firmware qmk
-	apt-get install -y gcc-avr avr-libc
+	git clone git@github.com:qmk/qmk_firmware.git qmk
+	(cd qmk && git checkout 7260fc3eef98fb7b0e2ed24d3d0d14cf2e613000)
+	apt-get install -y make gcc-avr avr-libc
 fi
 
 dest=qmk/keyboards/ergodox/keymaps/xudongz
